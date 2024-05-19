8d30ac10b6123b10254978b373045831cc1c3114
RedisculuS <Altunina311@gmail.com>
2024-05-19 22:44:26 +0300

    3rd commit
    finishing with pixabay
    still need to add simplelightbox + fixing styles for gallery cards

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,2 +1,34 @@
 'use strict';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
+export function renderImages(images) {
+  return images
+    .map(
+      img => `<li class="gallery-item">
+      <a class="gallery-link" href="${img.webformatURL}">
+        <img 
+          class="gallery-image" 
+          src="${img.largeImageURL}" 
+          alt="${img.tags}" 
+          />
+          
+      </a>
+      <ul class='description'>
+      <li><strong>Likes</strong> ${img.likes}</li>
+      <li><strong>Views</strong> ${img.views}</li>
+      <li><strong>Comments</strong> ${img.comments}</li>
+      <li><strong>Downloads</strong> ${img.downloads}</li>
+
+      </ul>
+    </li>`
+    )
+    .join('');
+}
+
+export function showError(message) {
+  iziToast.error({
+    title: 'Error',
+    message: message,
+  });
+}
--- src/main.js
+++ src/main.js
@@ -1,29 +1,46 @@
 'use strict';
 
-const form = document.querySelector(".form");
-const searchInput = document.querySelector(".search-img-input");
-const btnSubmit = document.querySelector(".submit-btn")
+import { fetchImages } from './js/pixabay-api';
+// import { renderImages, showError, clearError } from './js/render-functions';
+import { renderImages, showError } from './js/render-functions';
+
+const form = document.querySelector('.form');
+const searchInput = document.querySelector('.search-img-input');
+const gallery = document.querySelector('.gallery');
+// const btnSubmit = document.querySelector('.submit-btn');
 
 // console.log(form.children);
 
-form.addEventListener("input", handleInput)
-form.addEventListener("submit", handleSubmit);
+// form.addEventListener('input', handleInput);
+form.addEventListener('submit', handleSubmit);
 
-function handleInput(event) {
-    event.preventDefault();
-    
-}
-
+// function handleInput(event) {
+//   event.preventDefault();
+// }
 
 function handleSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
+  //   clearError();
 
-    const query = searchInput.value.trim();
+  const query = searchInput.value.trim();
 
-    if (!query) {
-        // alert
-        return;
-    }
+  if (!query) {
+    showError('Please enter a search term');
+    return;
+  }
 
-
+  fetchImages(query)
+    .then(images => {
+      if (images.length === 0) {
+        showError(
+          'Sorry, there are no images matching your search query. Please try again!'
+        );
+      } else {
+        gallery.innerHTML = renderImages(images);
+      }
+    })
+    .catch(error => {
+      showError('An error occured while fetching images');
+      console.error(error);
+    });
 }
